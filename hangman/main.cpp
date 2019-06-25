42eb52d2ac2034e3ff8ce02ed31ca0aef999b991
Brandon Burch <Brandon.burch12@gmail.com>
2019-06-25 14:16:05 -0500

    Updates

---
+++ hangman/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void PrintMessage(string message, bool printTop = true, bool printBottom = true)
+{
+    if (printTop)
+    {
+        cout << "+----------------------------+" << endl;
+        cout << "|";
+    }
+}
+int main()
+{
+
+    getchar();
+    return 0;
+}
